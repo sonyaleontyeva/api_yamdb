72396664fad71e7e293d4240c1e583eac446cebe
Sonya Leontyeva <sonya_leontyeva@mail.ru>
2023-06-18 13:01:46 +0300

    Edited permission IsAdmin

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,9 +4,6 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
     """Проверка доступа по роли admin."""
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_admin()
-
     def has_permission(self, request, view):
         return request.user.is_admin()
 
